00de2d5c2da4c736283e0372d3c8fbe7750d6724
Jean Garcia <jean_clg@hotmail.com>
2024-12-20 14:11:08 -0300

    feat: initialize "CLI-Feed" project

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Em Construção</title>
+        <meta name="description" content="Página em construção" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>🚧 Em Construção 🚧</h1>
+        <p>Estamos trabalhando para trazer algo incrível para você!</p>
+      </main>
+    </div>
+  );
+}
