ec34e2e496a89a59adf7699c0d900bea3fda0066
Jean Garcia <jean_clg@hotmail.com>
2025-01-14 18:38:45 -0300

    feat: add prisma client, update compose.yaml to use .env and update endpoint `/status` to check prisma connection

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,5 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next/types";
+import prisma from "../../../../lib/prisma";
 
-export default (_req: NextApiRequest, res: NextApiResponse) => {
+export default async (_req: NextApiRequest, res: NextApiResponse) => {
+  const prismaTest = await prisma.$queryRaw<{
+    sum: number;
+  }>`SELECT 1+1 as sum;`;
+
+  console.log("RESULTADO: ", prismaTest[0]);
+
   res.status(200).json({ status: "OK" });
 };
