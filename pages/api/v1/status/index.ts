3ad5f4965de2ccfbd16ccd7934c2e1d253bf8206
Jean Garcia <jean_clg@hotmail.com>
2025-01-16 18:09:17 -0300

    feat: add `.env.development` and add services scripts

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next/types";
-import prisma from "../../../../lib/prisma";
+import prisma from "lib/prisma";
 
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
   const prismaTest = await prisma.$queryRaw<{
