281e2bc1091f87c4f1c2470a88e102a8e6f0853f
Jean Garcia <jean_clg@hotmail.com>
2025-03-05 09:49:55 -0300

    feat: add database properties in `/api/v1/status` endpoint

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,12 +1,36 @@
 import { NextApiRequest, NextApiResponse } from "next/types";
 import prisma from "lib/prisma";
 
+type TResponse = {
+  updated_at: Date;
+  dependencies: {
+    database: {
+      version: string;
+      max_connections: number;
+      opened_connections: number;
+    };
+  };
+};
+
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
-  const prismaTest = await prisma.$queryRaw<{
-    sum: number;
-  }>`SELECT 1+1 as sum;`;
+  let result: TResponse;
 
-  console.log("RESULTADO: ", prismaTest[0]);
+  const databaseVersion = await prisma.$queryRaw`SHOW server_version;`;
+  const databaseMaxConnections =
+    await prisma.$queryRaw`SELECT current_setting('max_connections')::int AS max_connections;`;
+  const databaseOpenedConnections =
+    await prisma.$queryRaw`SELECT COUNT(*)::int FROM pg_stat_activity WHERE state = 'active';`;
 
-  res.status(200).json({ status: "OK" });
+  result = {
+    updated_at: new Date(),
+    dependencies: {
+      database: {
+        version: databaseVersion[0].server_version,
+        max_connections: databaseMaxConnections[0].max_connections,
+        opened_connections: databaseOpenedConnections[0].count,
+      },
+    },
+  };
+
+  res.status(200).json(result);
 };
--- tests/integrations/api/v1/status/get.test.ts
+++ tests/integrations/api/v1/status/get.test.ts
@@ -1,7 +1,14 @@
 describe("/api/v1/status", () => {
-  it("should return 200 status", async () => {
+  it("Status page", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
+    const responseData = await response.json();
 
+    const parsedDate = new Date(responseData.updated_at).toISOString();
+
+    expect(responseData.updated_at).toEqual(parsedDate);
+    expect(responseData.dependencies.database.version).toEqual("17.2");
+    expect(responseData.dependencies.database.max_connections).toEqual(100);
+    expect(responseData.dependencies.database.opened_connections).toEqual(1);
     expect(response.status).toBe(200);
   });
 });
